21e365462b0f21c3252dd574ea306082082b6f81
tammy vu <tammy.vsk@gmail.com>
2023-01-23 12:13:59 -0600

    completet

--- Abbreviate.java
+++ Abbreviate.java
@@ -1,5 +1,16 @@
 public class Abbreviate {
     public static void main(String[] args) {
-        
+        String a = args[0];
+        String b;
+
+        if (a.length() < 8){
+            b = a;
+            System.out.println(b);
+        }else{
+            int beforeLastLetter = a.length() - 2;
+            int lastLetter = a.length();
+            b = (a.substring(0,2) + "..." + a.substring(beforeLastLetter, lastLetter));
+            System.out.println(b);
+        }
     }
 }
