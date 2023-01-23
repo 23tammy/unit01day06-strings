41bd8ecc0f52d1a8c5067b0bcb9405e30e28319e
tammy vu <tammy.vsk@gmail.com>
2023-01-23 12:01:17 -0600

    complete

--- NegatePhrase.java
+++ NegatePhrase.java
@@ -1,5 +1,11 @@
 public class NegatePhrase {
     public static void main(String[] args) {
-        
+        String a = args[0];
+
+        if (a.substring(0,3).equals("not")) {
+            System.out.println(a);
+        }else{
+            System.out.println("not " + a);
+        }
     }
 }
